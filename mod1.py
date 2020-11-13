0d6a4a2b23edb08734bd75587daae1a5a8955f34
nikolska <nikolska1995@gmail.com>
2020-11-13 16:07:55 +0100

    version 2

---
+++ mod1.py
@@ -0,0 +1,85 @@
+from roll_the_dice import roll_the_dice
+import random
+
+
+def user_roll_the_dice():
+    '''
+    Function simulates rolling two dice which choose the player.
+    :return: the amount of rolls, type - int
+    '''
+    text = 'Choose a dice from the set: D3, D4, D6, D8, D10, D12, D20, D100.'
+    roll_twice = [roll_the_dice(input(text).upper()), roll_the_dice(input(text).upper())]
+    return sum(roll_twice)
+
+
+def computer_roll_the_dice():
+    '''
+    Function simulates rolling two random dice.
+    :return: the amount of rolls, type - int
+    '''
+    cube_types = ['D3', 'D4', 'D6', 'D8', 'D10', 'D12', 'D20', 'D100']
+    rand_choice = random.choice(cube_types)
+    roll_twice = [roll_the_dice(rand_choice), roll_the_dice(rand_choice)]
+    return sum(roll_twice)
+
+
+def user_round(points):
+    '''
+    Function checks the sum of the rolls.
+    :return: score result, type - int
+    '''
+    new_points = points
+    rolling = user_roll_the_dice()
+    if rolling == 7:
+        new_points //= 7
+    elif rolling == 11:
+        new_points *= 11
+    else:
+        new_points += rolling
+    return new_points
+
+
+def computer_round(points):
+    '''
+    Function checks the sum of the rolls.
+    :return: score result, type - int
+    '''
+    new_points = points
+    rolling = computer_roll_the_dice()
+    if rolling == 7:
+        new_points //= 7
+    elif rolling == 11:
+        new_points *= 11
+    else:
+        new_points += rolling
+    return new_points
+
+
+def game():
+    '''
+    Function, checks the results of the players' points, if the result
+    is equal to or greater than 2001 - the game is finished, the player has won.
+    :user_points type: int
+    :computer_points type: int
+    '''
+    user_points = 0
+    computer_points = 0
+    while user_points < 2001 or computer_points < 2001:
+        print('Press ENTER to roll the dice')
+        input()
+        user_points = user_round(user_points)
+        print('user points =', user_points)
+
+        computer_points = computer_round(computer_points)
+        print('compute points =', computer_points)
+
+        if user_points >= 2001:
+            print('You win!')
+            break
+        elif computer_points >= 2001:
+            print('I win!')
+            break
+
+
+if __name__ == '__main__':
+    game()
---
+++ roll_the_dice.py
@@ -0,0 +1,60 @@
+import random
+
+
+def roll_the_dice(data):
+    '''
+    Function roll_the_dice takes a string, recognizes the type of dice, number of rolls,
+    modifier, and if the given string is incorrect, it will return the appropriate message,
+    otherwise it will simulate the rolls and return the result.
+    :data type: string
+    :return: result of the dice roll if data is correct, or error-message if not.
+    '''
+    cube_types = ['3', '4', '6', '8', '10', '12', '20', '100']
+    list_of_data = [i for i in data]
+    first_part = 0
+    last_part = 0
+    action = ''
+    number_of_dice = 0
+    type_of_dice = 0
+    answer = 0
+    for i in list_of_data:
+        if '+' in list_of_data:
+            first_part = data.split('+')[0]
+            last_part = data.split('+')[-1]
+            action = '+'
+        elif '-' in list_of_data:
+            first_part = data.split('-')[0]
+            last_part = data.split('-')[-1]
+            action = '-'
+        elif '+' or '-' not in data:
+            first_part = data
+            last_part = 0
+    for i in first_part:
+        if 'D' in first_part:
+            number_of_dice = first_part.split('D')[0]
+            if not number_of_dice:
+                number_of_dice = 1
+            type_of_dice = first_part.split('D')[-1]
+        else:
+            return 'The string is not valid!'
+    if type_of_dice not in cube_types:
+        return 'The string is not valid!'
+    rand_num = random.randint(1, int(type_of_dice))
+    if action == '+':
+        answer = int(number_of_dice) * rand_num + int(last_part)
+    elif action == '-':
+        answer = int(number_of_dice) * rand_num - int(last_part)
+    else:
+        answer = int(number_of_dice) * rand_num
+    return answer
+
+
+if __name__ == '__main__':
+    print(roll_the_dice('2D10+10'))
+    print(roll_the_dice('2D3'))
+    print(roll_the_dice('D6'))
+    print(roll_the_dice('D12-1'))
+    print(roll_the_dice('D54-1'))
+    print(roll_the_dice('5451'))
+    print(roll_the_dice('D12/1'))
+    print(roll_the_dice('5B68-1'))
