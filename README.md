9e7a3c35d4214352cfa32eb9075ceb5974bc3bfb
nikolska <nikolska1995@gmail.com>
2020-11-13 14:10:54 +0100

    simplify version

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Games-room-2001
-Playing with dice and math
+
+Each player starts with 0 points.
+
+On his turn, the player rolls 2 dice (standard six-sided dice).
+
+The number of spots thrown is added to the total number of points.
+
+Starting from the second round:
+
+if a player rolls a 7, he divides his number of points by that value, discarding the fraction
+
+if he rolls an 11, he multiplies the current number of points by that value.
+
+The first player to score 2001 points wins.
+
+To run the program, just open the "main.py" file in the console or open the "main.py" file in the IDE and press "run main".
--- main.py
+++ main.py
@@ -1,56 +1,55 @@
 import random
 
 
-def user_roll_the_dice():
-    '''
-    Function simulates rolling two six-sided dice.
-    :return: the amount of rolls, type - int
-    '''
-    print('Press ENTER to roll the dice')
-    input()
-    roll_twice = [random.randint(1, 6), random.randint(1, 6)]
-    return sum(roll_twice)
-
-
-def comp_roll_the_dice():
+def roll_the_dice():
     '''
     Function simulates rolling two six-sided dice.
     :return: the amount of rolls, type - int
     '''
     roll_twice = [random.randint(1, 6), random.randint(1, 6)]
     return sum(roll_twice)
+
+
+def game_rounds(points):
+    '''
+    Function checks the sum of the rolls: if the sum = 7, the points are divided
+    without a remainder by 7, if the sum = 11 - the points are multiplied by 11,
+    if the sum is any other number - the sum is added to the points.
+    :return: score result, type - int
+    '''
+    new_points = points
+    rolling = roll_the_dice()
+    if rolling == 7:
+        new_points //= 7
+    elif rolling == 11:
+        new_points *= 11
+    else:
+        new_points += rolling
+    return new_points
 
 
 def game():
     '''
-    The main function of the game "2001".
+    The main function, checks the results of the players' points, if the result
+    is equal to or greater than 2001 - the game is finished, the player has won.
     :user_points type: int
     :computer_points type: int
     '''
     user_points = 0
     computer_points = 0
-    while user_points < 200 or computer_points < 200:
-        user_roll = user_roll_the_dice()
-        if user_roll == 7:
-            user_points //= 7
-        elif user_roll == 11:
-            user_points *= 11
-        else:
-            user_points += user_roll
-        print('user_points =', user_points)
-        if user_points >= 200:
+    while user_points < 2001 or computer_points < 2001:
+        print('Press ENTER to roll the dice')
+        input()
+        user_points = game_rounds(user_points)
+        print('user points =', user_points)
+
+        computer_points = game_rounds(computer_points)
+        print('compute points =', computer_points)
+
+        if user_points >= 2001:
             print('You win!')
             break
-
-        computer_roll = comp_roll_the_dice()
-        if computer_roll == 7:
-            computer_points //= 7
-        elif computer_roll == 11:
-            computer_points *= 11
-        else:
-            computer_points += computer_roll
-        print('computer_points =', computer_points)
-        if computer_points >= 200:
+        elif computer_points >= 2001:
             print('I win!')
             break
 
